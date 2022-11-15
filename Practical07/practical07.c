438e649d9af93fd4c9a2fd97505d200bcad8f83e
eoinw1 <donal.whelan1@ucdconnect.ie>
2022-11-15 20:43:14 +0000

    Practical07 submission for computing taylor series

---
+++ Practical07/practical07.c
@@ -0,0 +1,53 @@
+/* 
+    Find e using Taylor series expansion  
+*/
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+int factorial(int n);
+
+int main(void){
+
+    int i, order;
+    double e, *terms;
+
+//Enter Polynomial order
+    printf("Please enter the required polynomial order\n");
+    if (scanf("%d", &order) !=1){
+        printf("Error, Did not enter a number");
+        return 1;
+    }
+
+//Allocate space for terms array
+    terms=malloc(order*sizeof(double));
+    for (i=0; i<order; i++){
+        terms[i]=1.0/(double)factorial(i+1);
+        printf("e term for order %d is %1.14lf\n", order, terms[i]);
+    }
+
+    e=1.0;
+    for(i=0; i<order; i++){
+        e=e+terms[i];
+    }
+
+    free(terms);
+
+    printf("\ne is estimated as %1.14lf, with difference %1.14lf\n", e, e-exp(1.0));
+
+    return 0;
+}
+
+int factorial(int n){
+    if(n<0){
+        printf("Error: Input should be positive value ");
+        return(-1);
+    }
+    else if (n==0){
+        return 1;
+    }
+    else{
+        return(n*factorial(n-1));
+    }
+}
---
+++ Practical07/readme.md
@@ -0,0 +1,3 @@
+practical07 takes a user inputed positive integer and prints the Taylor series for that order. It does this using a dynamically allocated array. At orders above 13! integers cannot hold such high values 
+
+dynamic is a practice program for filling and printing from dynamic arrays using pointers in C.
